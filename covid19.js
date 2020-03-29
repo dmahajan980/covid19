b1123d1ae64a4b0f0cb98645aece2bce750bbb6d
dmahajan980 <divyanshumahajan98@gmail.com>
2020-03-29 21:51:08 +0530

    Added feature to fetch global stats

--- covid19.js
+++ covid19.js
@@ -1,4 +1,9 @@
 const program = require('commander'),
+    request = require('request'),
+    Table = require('cli-table3'),
+    Spinner = require('cli-spinner').Spinner,
+    cursor = require('cli-cursor'),
+    chalk = require('chalk'),
     {
         version,
         description
@@ -7,5 +12,53 @@ const program = require('commander'),
 program
     .version(version, '-v, --version', 'Display the current version of the program')
     .description(description);
+
+// Country
+program
+    .command('stats')
+    .alias('s')
+    .description('Returns the COVID-19 statistics')
+    .usage('stats [options]')
+    .action(() => {
+        cursor.hide();
+        let spinner = new Spinner(chalk.rgb(74, 177, 248)('%s'));
+        spinner.setSpinnerString(18);
+        spinner.start();
+
+        const configuration = {
+            url: 'https://covidapi.info/api/v1/global',
+            json: true
+        };
+
+        request(configuration, (error, response) => {
+            console.log('\n');
+            
+            if (error) {
+                console.error(chalk.bold.red('Unable to connect :('));
+                return spinner.stop();
+            }
+
+            const { 
+                    confirmed,
+                    deaths,
+                    recovered 
+                } = response.body.result,
+                country = 'Worldwide';
+
+            let table = new Table({
+                head: [chalk.bold.hex('#8956ff')(country), chalk.bold.yellowBright('No of Cases')]
+            });
+            
+            table.push(
+                [ chalk.red.bold('Confirmed'), chalk.whiteBright.bold(confirmed) ],
+                [ chalk.bold.blue('Deaths'), chalk.whiteBright.bold(deaths) ],
+                [ chalk.bold.green('Recovered'), chalk.whiteBright.bold(recovered) ]
+            );
+            console.log(table.toString());
+
+            spinner.stop();
+            cursor.show();
+        });
+    })
 
 program.parse(process.argv);
